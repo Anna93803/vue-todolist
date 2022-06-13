28c744345e58f27b864181aee4b4a196c781ddf6
Anna93803 <anna.tassone93@gmail.com>
2022-06-13 19:05:38 +0200

    add bonus 1

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@
 // cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
 // che quindi viene aggiunto alla lista dei todo esistenti
 
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
 const todos = [
     {
         text: "Fare la spesa",
@@ -42,17 +46,19 @@ const todos = [
 const appVue = new Vue ({
 
     el: "#app",
-
+    // nel data ho trasferito l'array di oggetti e salvato in una variabile, ho creato poi una variabile con stringa vuota che miservirà per il campo input
     data: {
         listTodo: todos,
         addTextTodo: "",
     },
 
     methods: {
+        // ho creato una funzione che al click sulla x viene eliminato tutto l'oggetto selezionato
         deleteTodo(index) {
             this.listTodo.splice(index, 1);
         },
-
+        // ho creato un altra funzione dove tutto quello inserito nell'input testuale viene aggiunto alla lista dei todolist, con le sue proprietà
+        // ho fatto un controllo che se il campo è vuoto e si clicca su aggiungi, non viene aggiunto nulla nella lista
         addTodo() {
 
             if(this.addTextTodo === "") {
@@ -63,7 +69,7 @@ const appVue = new Vue ({
                 text: this.addTextTodo,
                 done: false,
             })
-
+            // infine ho svutato il campo
             this.addTextTodo = ""
         }
     }
